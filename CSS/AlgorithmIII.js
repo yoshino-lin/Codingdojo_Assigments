ce7ac2b5679e4453b9a72ee33bdb0daeb8075f10
yudong <yoshino1347716570@gmail.com>
2019-09-23 11:05:38 +0800

    easy...??

---
+++ CSS/AlgorithmIII.js
@@ -0,0 +1,34 @@
+function printAverage(x){
+   sum = 0;
+   for(var i=0; i<x.length; i++){
+       sum+=x[i];
+   }
+   return sum/x.length;
+}
+y = printAverage([1,2,3]);
+console.log(y); // should log 2
+y = printAverage([2,5,8]);
+console.log(y); // should log 5
+
+
+function returnOddArray(){
+   var arr = [];
+   for(var i = 1; i<=255; i+=2){
+       arr.push(i);
+   }
+   return arr;
+}
+y = returnOddArray();
+console.log(y); // should log [1,3,5,...,253,255]
+
+
+function squareValue(x){
+    for(var i=0; i<x.length; i++){
+        x[i]*=x[i];
+    }
+   return x;
+}
+y = squareValue([1,2,3]);
+console.log(y); // should log [1,4,9]
+y = squareValue([2,5,8]);
+console.log(y); // should log [4,25,64]
